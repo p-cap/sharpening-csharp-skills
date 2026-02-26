c3a5c8615f7e1f7564dd5a0719a0f24e8ecbd3b8
p-cap <slashedeye@gmail.com>
2026-02-26 04:56:49 -0600

    Maui App number 2. Same as the first but repitition is key

---
+++ 2 - Feb/26/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Phoneword;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	string? translatedNumber;
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            // TODO:
+			CallButton.IsEnabled = true;
+        	CallButton.Text = "Call " + translatedNumber;
+        }
+        else
+        {
+            // TODO:
+			CallButton.IsEnabled = false;
+        	CallButton.Text = "Call";
+        }
+    }
+
+	async void OnCall(object sender, System.EventArgs e)
+    {
+		if (await this.DisplayAlertAsync(
+			"Dial a Number",
+			"Would you like to call " + translatedNumber + "?",
+			"Yes",
+			"No"))
+		{
+        // TODO: dial the phone
+		try
+			{
+				if (PhoneDialer.Default.IsSupported && !string.IsNullOrWhiteSpace(translatedNumber))
+                PhoneDialer.Default.Open(translatedNumber);
+			}
+		catch (ArgumentNullException)
+			{
+				await DisplayAlertAsync("Unable to dial", "Phone number was not valid.", "OK");
+			}
+		catch (Exception)
+			{
+			// Other error has occurred.
+            await DisplayAlertAsync("Unable to dial", "Phone dialing failed.", "OK");
+			}
+
+    	}
+	}
+
+}
