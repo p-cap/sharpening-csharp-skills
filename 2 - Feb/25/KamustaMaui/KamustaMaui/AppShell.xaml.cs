b975da179bfc62d6bffa5755c6bee547ba6d67c0
p-cap <slashedeye@gmail.com>
2026-02-25 18:19:25 -0600

    .net maui boilerplate

---
+++ 2 - Feb/25/KamustaMaui/KamustaMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KamustaMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 2 - Feb/25/KamustaMaui/KamustaMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KamustaMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
